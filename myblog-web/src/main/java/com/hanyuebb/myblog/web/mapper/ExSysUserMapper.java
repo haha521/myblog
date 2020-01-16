07fa198ca84f0052d7a00295dce3a2972fac5408
haha521 <317909547@qq.com>
2020-01-17 00:33:51 +0800

    bnbn

--- myblog-web/src/main/java/com/hanyuebb/myblog/web/mapper/ExSysUserMapper.java
+++ myblog-web/src/main/java/com/hanyuebb/myblog/web/mapper/ExSysUserMapper.java
@@ -2,6 +2,10 @@ package com.hanyuebb.myblog.web.mapper;
 
 import com.hanyuebb.myblog.common.domain.SysUser;
 import com.hanyuebb.myblog.common.utils.MyMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface ExSysUserMapper extends MyMapper<SysUser> {
 }
